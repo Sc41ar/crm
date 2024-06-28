f3595d3b681ecf076ed735a1efc7020191bff8f4
Nikita <mrniki2396@gmail.com>
2024-06-29 03:08:03 +0700

    contacts creation

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const Input = ({ id, placeholder, required, type }) => {
-  return <input id={id} placeholder={placeholder} required={required} type={type} />;
+const Input = ({ id, placeholder, required, type, onChange }) => {
+  return (
+    <input
+      onChange={onChange}
+      id={id}
+      placeholder={placeholder}
+      required={required}
+      type={type}
+      className="rounded-md bg-indigo-300 dark:bg-indigo-900 hover:bg-purple-300 hover:dark:bg-purple-900 p-2"
+    />
+  );
 };
 
 export default Input;
