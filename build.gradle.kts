a9d9c7f0704b0ef0469b5162fe05d66f6e574055
Nikita <nik_naydenko@mail.ru>
2024-07-02 20:25:41 +0700

    класс для mail

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-mail:3.3.1")
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-data-jpa
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.0")
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
